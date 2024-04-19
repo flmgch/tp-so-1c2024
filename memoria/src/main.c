beada93e82e7ce8373aa9800e1321765177657b6
mSpadoni <mspadoni@frba.utn.edu.ar>
2024-04-19 10:58:26 +0000

    arreglado problemas con conexiones

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -14,13 +14,13 @@ int main(int argc, char *argv[])
   int socket_escucha = iniciar_escucha(puerto_escucha, "Memoria", mem_logger);
   
   // SOCKETS ENTRANTES
-  int socket_cpu = esperar_conexion(socket_Escucha, "CPU", mem_logger);
-  int socket_kernel = esperar_conexion(socket_Escucha, "Kernel", mem_logger);
-  int socket_interfaz = esperar_conexion(socket_Escucha, "Interfaz", mem_logger);
+  int socket_cpu = esperar_conexion(socket_escucha, "CPU", mem_logger);
+  int socket_kernel = esperar_conexion(socket_escucha, "Kernel", mem_logger);
+  int socket_interfaz = esperar_conexion(socket_escucha, "Interfaz", mem_logger);
 
-  close(socket_Escucha);
+  close(socket_escucha);
   close(socket_interfaz);
-  close(socket_CPU);
+  close(socket_cpu);
   close(socket_kernel);
   terminar_programa(mem_logger, mem_config);
   return 0;
