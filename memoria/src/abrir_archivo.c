d84bb39f3cf0ed6b281a4c81cefa6aac93cd1bc9
Joaquin Rossi <joaquinnrossi@gmail.com>
2024-06-14 01:28:57 +0000

    Arreglado crear_proceso en memoria

--- memoria/src/abrir_archivo.c
+++ memoria/src/abrir_archivo.c
@@ -5,15 +5,13 @@ t_list *abrir_archivo(const char *file)
     FILE *pseudocodiogo = fopen(file, "r");
     if (pseudocodiogo != NULL)
     {
-        int posicion = 0;
         char instruccion[35];
         t_list *lista_de_instrucciones = list_create();
         while (!feof(pseudocodiogo))
         {
             fgets(instruccion, 35, pseudocodiogo);
             log_info(mem_logger, "%s", instruccion);
-            list_add_in_index(lista_de_instrucciones, posicion, instruccion);
-            posicion++;
+            list_add(lista_de_instrucciones, instruccion);
         }
         fclose(pseudocodiogo);
         return lista_de_instrucciones;
--- memoria/src/crear_proceso.c
+++ memoria/src/crear_proceso.c
@@ -8,11 +8,16 @@ t_proceso *atender_crear_proceso(t_buffer *buffer)
     proceso->pid = pid;
     proceso->path = path;
 
-    char *archivo = strcat(path_instrucciones, proceso->path);
+    char *auxiliar = malloc(strlen(path_instrucciones) + 1);
+
+    strcpy(auxiliar, path_instrucciones);
+
+    char *archivo = strcat(auxiliar, path);
 
     proceso->listas = abrir_archivo(archivo);
     proceso->size = 0;
     proceso->filas_tabla_paginas = list_create();
 
+    free(auxiliar);
     return proceso;
 };
