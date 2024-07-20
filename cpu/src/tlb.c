827b8e2535d6273df2807f2a6ff68c3f5060288a
utnso <mdibucci@frba.utn.edu.ar>
2024-07-20 16:18:08 +0000

    correcciones tlb

--- cpu/src/tlb.c
+++ cpu/src/tlb.c
@@ -1,6 +1,7 @@
 #include "tlb.h"
 
 int tlb_buscar(uint32_t pid, int numero_pagina){
+    contador_acceso++;
     for (int i = 0; i < tlb_entradas; ++i) {
         if (tlb[i].pid == pid && tlb[i].pagina == numero_pagina) {
             return i; 
@@ -15,7 +16,7 @@ void tlb_agregar(uint32_t pid, int pagina, int marco) {
         tlb[tlb_entradas].pid = pid;
         tlb[tlb_entradas].pagina = pagina;
         tlb[tlb_entradas].marco = marco;
-        tlb[tlb_entradas].ultimo_acceso = contador_acceso++; //para el lru
+        tlb[tlb_entradas].ultimo_acceso = contador_acceso; // para el lru
         tlb_entradas++;
     } else {
         //no tengo espacio, sacrifico una entrada
@@ -32,9 +33,8 @@ void tlb_agregar(uint32_t pid, int pagina, int marco) {
         tlb[index_reemplazo].pid = pid;
         tlb[index_reemplazo].pagina = pagina;
         tlb[index_reemplazo].marco = marco;
-        tlb[index_reemplazo].ultimo_acceso = contador_acceso++; //para el lru
-        tlb_proximo_reemplazo = (tlb_proximo_reemplazo + 1) % cantidad_entradas_tlb;
-        // para el fifo
+        tlb[index_reemplazo].ultimo_acceso = contador_acceso;                        // para el lru
+        tlb_proximo_reemplazo = (tlb_proximo_reemplazo + 1) % cantidad_entradas_tlb; // para el fifo
     }
 }
 
@@ -42,10 +42,11 @@ int encontrar_entrada_lru() {
     int lru_index = 0;
     int min_acceso = tlb[0].ultimo_acceso;
 
-    for (int i = 1; i < tlb_entradas; ++i) { //busco la entrada cn menor acceso para sacrificar
+    for (int i = 0; i < tlb_entradas; ++i)
+    { // busco la entrada cn menor acceso (accedido hace mas) para sacrificar
         if (tlb[i].ultimo_acceso < min_acceso) {
             min_acceso = tlb[i].ultimo_acceso;
-            lru_index = i;
+            lru_index = min_acceso;
         }
     }
 
